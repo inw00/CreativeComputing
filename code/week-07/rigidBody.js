25bd18d2c196b69e34473dfd14c0169ddfe689a4
inw00 <inwoobaek408@gmail.com>
2024-02-29 20:24:48 -0500

    angry birds

---
+++ code/week-07/rigidBody.js
@@ -0,0 +1,38 @@
+class rigidBody {
+    constructor(x,y, size) {
+        this.loc = createVector(x,y)
+        this.vel = createVector();
+        this.acc = createVector();
+        this.friction = 0.985;
+        this.size = size;
+    }
+
+    applyForce(force) {
+        this.acc.add(force);
+    }
+
+    update() {
+        this.vel.add(this.acc);
+        this.loc.add(this.vel);
+        this.acc.mult(0);
+        this.vel.mult(this.friction);
+    }
+
+    display() {
+        circle(this.loc.x, this.loc.y, this.size);
+    }
+
+    bounce() {
+        let top = this.size/2;
+        let bottom = height - this.size/2;
+        let left = this.size/2;
+        let right = width - this.size/2;
+
+        if (this.loc.y < top || this.loc.y > bottom) {
+            this.vel.y *= -1;
+        }
+        if (this.loc.x < left || this.loc.x > right) {
+            this.vel.x *= -1;
+        }
+    }
+}
